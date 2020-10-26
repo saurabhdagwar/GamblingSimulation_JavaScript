cd6e353a85146f045eca27e4b276f9d370f0992d
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-10-26 21:29:01 +0530

    add: Resign game when gambler win or lost 50% of stake

--- Gambling.js
+++ Gambling.js
@@ -1,18 +1,20 @@
 //Constants
-const STACK_AMOUNT = 100 ;
+const STAKE_AMOUNT = 100 ;
 const BETTING_AMOUNT = 1 ;
 //Variables
 let game_result;
 
 function GamblingGame(){
-    game_result = STACK_AMOUNT;
-    var game_check = Math.floor( Math.random() * 2 );
-    if(game_check == BETTING_AMOUNT){
-        game_result = game_result + BETTING_AMOUNT ;
+    game_result = STAKE_AMOUNT;
+    while( game_result > ( STAKE_AMOUNT / 2 ) && game_result < ( STAKE_AMOUNT + ( STAKE_AMOUNT/2 ))){
+        var game_check = Math.floor( Math.random() * 2 );
+        if(game_check == BETTING_AMOUNT){
+            game_result = game_result + BETTING_AMOUNT ;
+        }
+        else{
+            game_result = game_result - BETTING_AMOUNT ;
+        }
+        console.log(`Game Result ${game_result}`);
     }
-    else{
-        game_result = game_result - BETTING_AMOUNT ;
-    }
-    console.log(`Game Result ${game_result}`);
 }
 GamblingGame();
