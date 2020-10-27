5ff9bab6e9fe4807e95740ddd5eff7b1a0fe09a5
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-10-27 19:19:42 +0530

    refactor: Calculate betting amount for either win or loose

--- Gambling.js
+++ Gambling.js
@@ -8,10 +8,10 @@ function GamblingGame(){
     game_result = STACK_AMOUNT;
     var game_check = Math.floor( Math.random() * 2 );
     if(game_check == BETTING_AMOUNT){
-        game_result = game_result + BETTING_AMOUNT ;
+        game_result = game_result + BETTING_AMOUNT;
     }
     else{
-        game_result = game_result - BETTING_AMOUNT ;
+        game_result = game_result - BETTING_AMOUNT;
     }
     console.log(`Game Result ${game_result}`);
 }
